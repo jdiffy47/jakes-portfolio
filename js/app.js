83d9461679973874c88dc8a2831e958bfadd41e8
JakeD <jakediefenderfer@gmail.com>
2022-06-07 22:21:05 -0400

    create cached element references

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('sanity check')
+// CACHED ELEMENT REFERENCES
+const homeBtn = document.getElementById('home-section')
+const workBtn = document.getElementById('work-section')
+const resumeBtn = document.getElementById('resume-section')
+const aboutBtn = document.getElementById('about-section')
+const contactBtn = document.getElementById('contact-section')
