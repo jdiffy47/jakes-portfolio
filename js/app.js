f3dbd85ee47e6f38f5a236a32fbef7d79c22fa61
JakeD <jakediefenderfer@gmail.com>
2022-06-08 15:41:23 -0400

    add contact links on contact page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // CACHED ELEMENT REFERENCES
 const homeBtn = document.getElementById('home-section')
-const workBtn = document.getElementById('work-section')
+const projectBtn = document.getElementById('project-section')
 const resumeBtn = document.getElementById('resume-section')
 const aboutBtn = document.getElementById('about-section')
 const contactBtn = document.getElementById('contact-section')
