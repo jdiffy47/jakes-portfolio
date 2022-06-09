0ff49df97d340fa044e7e1cc36c6743fcdf83bf2
JakeD <jakediefenderfer@gmail.com>
2022-06-09 10:00:14 -0400

    add external js file and append it to projects page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
+// IMPORT DATA HERE
+import { projectData } from "./project-data.js"
+
+
 // CACHED ELEMENT REFERENCES
 const homeBtn = document.getElementById('home-section')
 const projectBtn = document.getElementById('project-section')
 const resumeBtn = document.getElementById('resume-section')
 const aboutBtn = document.getElementById('about-section')
 const contactBtn = document.getElementById('contact-section')
+const projectContainer = document.getElementById('project-container')
+
+
+
+
+let projectMarkup = projectData.map(project =>
+  `
+      <img id="project-img" src="${project.image}" alt="">
+      <h3 id="project-title">${project.title}</h3>
+      <a id="project-github" href="${project.github}"></a>
+      <a id="project-deploy" href="${project.deployment}"></a>
+      <p id=#"project-descriptioni>${project.desciption}</p>
+  `
+  
+  ).join('')
+
+  projectContainer.innerHTML = projectMarkup
---
+++ js/project-data.js
@@ -0,0 +1,13 @@
+let projectData = [
+  {
+    title: 'Tic Tac Toe',
+    deployment: 'https://tic-tac-toe-jdiffy.netlify.app/',
+    image: '/assets/images/tic-tac-toe.jpg',
+    github: 'https://github.com/jdiffy47/ttt-weekend',
+    desciption: 'A game to outsmart your opponent'
+  }
+]
+
+export {
+  projectData
+}
