2bd719d85c71ffaf1221db20084174d447dae396
JakeD <jakediefenderfer@gmail.com>
2022-07-01 07:49:40 -0400

    add hushhush project to js file

--- js/project-data.js
+++ js/project-data.js
@@ -5,8 +5,14 @@ let projectData = [
     image: '/assets/images/tic-tac-toe.jpg',
     github: 'https://github.com/jdiffy47/ttt-weekend',
     desciption: 'A game to outsmart your opponent'
+  },
+  {
+    title: 'HushHush',
+    deployment: 'https://hushhush-app.herokuapp.com/',
+    image: 'https://i.imgur.com/ScbK6gN.jpg',
+    github: 'https://github.com/jdiffy47/hushhush-app',
+    desciption: 'A app to list your products or services'
   }
-  
 ]
 
 export {
